4e0f2501be7826171730e424f7d0aa98745ecedf
Roberto Ornelas <rob@front.no>
2013-10-28 16:17:30 +0000

    #4905 Mobile main menu styling/UX

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,15 @@
+// Custom
+jQuery(document).ready(function($) {
+  // Fastclick
+  FastClick.attach(document.body);
 
+  // Main menu
+  // Mobile dropdown
+  if ($('.nav--main').length) {
+    var $navMain = $('.nav--main');
+
+    $('h2', $navMain).click(function() {
+      $('.menu--main').slideToggle(300);
+    });
+  };
+});
